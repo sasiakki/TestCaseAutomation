48aa33a36cefd55393c521661b11eb16ec99c81d
Aishwarya - PTG <aishwarya.saibewar@peopletech.com>
2024-10-01 11:58:26 -0700

    Initial test automation code

---
+++ testcase.js
@@ -0,0 +1,4 @@
+{
+  "statusCode": 500,
+  "body": "\"Error fetching code from S3: An error occurred (AccessDenied) when calling the GetObject operation: User: arn:aws:sts::913524913171:assumed-role/createTestCases12-role-f3yukhaq/createTestCases12 is not authorized to perform: s3:GetObject on resource: \\\"arn:aws:s3:::testautomation-12/code.html\\\" because no identity-based policy allows the s3:GetObject action\""
+}
