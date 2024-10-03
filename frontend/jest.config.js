e47c04a66111a0a3de355d25bb192959a6a50331
peoplesnik <nikhil.sana@peopletech.com>
2024-10-02 22:25:46 -0700

    Update jest.config.js

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,15 +1,8 @@
 module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js'
-  },
   transform: {
     '^.+\\.(js|jsx)$': 'babel-jest'
   },
-  transformIgnorePatterns: [
-    '/node_modules/',
-    '^.+\\.module\\.(css|sass|scss)$'
-  ]
+  testMatch: ['**/testcase.js']
 };
